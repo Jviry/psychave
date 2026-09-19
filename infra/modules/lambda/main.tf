f2ef2beb343ace83eb449d0ee23720d72e8a6c18
not-sol <castones.sol@gmail.com>
2026-09-19 16:58:58 +0800

    feat: dev environment progress

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -18,6 +18,28 @@ resource "aws_iam_role" "lambda" {
   })
 }
 
-resource "aws_lambda_function" "api" {
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = var.source_dir
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "this" {
   function_name = var.function_name
+  role          = aws_iam_role.lambda.arn
+
+  runtime = var.runtime
+  handler = "main.handler"
+
+  filename         = data.archive_file.lambda.output_path
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  depends_on = [
+    aws_iam_role_policy_attachment.basic_execution
+  ]
 }
