d9e8e5ce54049f1210cb9efa40c245f5e01240d2
not-sol <castones.sol@gmail.com>
2026-09-25 15:33:26 +0800

    chore: remove Dockerfile and unused custom err middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
 from common.settings import settings
-from common.middleware.error_middleware import register_error_handlers
 from controllers.app_router import app_router
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
@@ -18,8 +17,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-register_error_handlers(app)
 
 
 @app.get("/health", tags=["Health"])
